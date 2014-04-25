e2a692a0b0e345528806b2dbe7f7d7791f9c494c
juanpleo1983 <juanpleo1983@gmail.com>
2014-04-25 16:23:23 +0200

    Comunicación pedidos pedientes hecho

---
+++ RestauranteCamarero/src/prg/pi/restaurantecamarero/decodificador/DecodificadorPedidosPendientesCamarero.java
@@ -0,0 +1,58 @@
+package prg.pi.restaurantecamarero.decodificador;
+import java.util.ArrayList;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import prg.pi.restaurantecamarero.restaurante.PedidosPendientesCamarero;
+import prg.pi.restaurantecamarero.restaurante.Producto;
+
+
+public class DecodificadorPedidosPendientesCamarero {
+	
+	private Document dom;
+	private ArrayList<PedidosPendientesCamarero> pedidosPendientes;
+	
+	public DecodificadorPedidosPendientesCamarero(Document dom){
+		this.dom = dom;
+		pedidosPendientes = new ArrayList<PedidosPendientesCamarero>();
+		generarPedidos();
+	}
+	
+	public PedidosPendientesCamarero[] getPedidosPendientes(){
+		return pedidosPendientes.toArray(new PedidosPendientesCamarero[0]);
+	}
+	
+	private void generarPedidos(){
+		/* Obtenemos los datos de la mesa */
+		NodeList nodeListMesa = dom.getElementsByTagName("mesa");
+		Node nodeMesa = nodeListMesa.item(0);
+		String nombreMesa = nodeMesa.getFirstChild().getNodeValue();
+		NodeList nodeListSeccion = dom.getElementsByTagName("seccion");
+		Node nodeSeccion = nodeListSeccion.item(0);
+		String nombreSeccion = nodeSeccion.getFirstChild().getNodeValue();
+		
+		/* Extraemos la información de los pedidos */
+		NodeList nodeListPedidos = dom.getElementsByTagName("pedidos");
+		Node nodePedidos = nodeListPedidos.item(0);
+		Element elementoPedidos = (Element) nodePedidos;
+		int idComanda = Integer.parseInt(elementoPedidos.getAttribute("idCom"));
+		
+		NodeList listaPedidos = dom.getElementsByTagName("pedido");
+		for(int contadorPedidos = 0; contadorPedidos < listaPedidos.getLength(); contadorPedidos++){
+			Node nodePedido = listaPedidos.item(contadorPedidos);
+			Element elementoPedido = (Element) nodePedido;
+			int idMenu = Integer.parseInt(elementoPedido.getAttribute("idMenu"));
+			NodeList nodeListDatos = nodePedido.getChildNodes();
+			String nombreProducto = nodeListDatos.item(0).getFirstChild().getNodeValue();
+            String nombreCantidad = nodeListDatos.item(1).getFirstChild().getNodeValue();
+            int unidades = Integer.parseInt(nodeListDatos.item(2).getFirstChild().getNodeValue());
+            int listos = Integer.parseInt(nodeListDatos.item(3).getFirstChild().getNodeValue());
+            int servidos = Integer.parseInt(nodeListDatos.item(4).getFirstChild().getNodeValue());
+            
+            pedidosPendientes.add(new PedidosPendientesCamarero(nombreSeccion, nombreMesa, idComanda, new Producto(idMenu, nombreProducto, nombreCantidad), listos, servidos));
+		}
+	}
+}
---
+++ RestauranteCamarero/src/prg/pi/restaurantecamarero/restaurante/PedidosPendientesCamarero.java
@@ -0,0 +1,71 @@
+package prg.pi.restaurantecamarero.restaurante;
+
+public class PedidosPendientesCamarero {
+	
+	private String nombreSeccion;
+	private String nombreMesa;
+	private int idComanda;
+	private Producto producto;
+	private int listos;
+	private int servidos;
+	
+	public PedidosPendientesCamarero(String nombreSeccion, String nombreMesa, int idComanda, Producto producto, int listos, int servidos){
+		this.nombreSeccion= nombreSeccion;
+		this.nombreMesa = nombreMesa;
+		this.idComanda = idComanda;
+		this.producto = producto;
+		this.listos = listos;
+		this.servidos = servidos;
+	}
+
+	public String getNombreSeccion() {
+		return nombreSeccion;
+	}
+
+	public void setNombreSeccion(String nombreSeccion) {
+		this.nombreSeccion = nombreSeccion;
+	}
+
+	public String getNombreMesa() {
+		return nombreMesa;
+	}
+
+	public void setNombreMesa(String nombreMesa) {
+		this.nombreMesa = nombreMesa;
+	}
+
+	public int getIdComanda() {
+		return idComanda;
+	}
+
+	public void setIdComanda(int idComanda) {
+		this.idComanda = idComanda;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public int getListos() {
+		return listos;
+	}
+
+	public void setListos(int listos) {
+		this.listos = listos;
+	}
+
+	public int getServidos() {
+		return servidos;
+	}
+
+	public void setServidos(int servidos) {
+		this.servidos = servidos;
+	}
+
+	
+
+}
