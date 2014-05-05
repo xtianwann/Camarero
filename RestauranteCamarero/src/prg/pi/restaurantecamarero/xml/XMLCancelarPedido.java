14bb9d1a5e4a9d4576c9241a8902b9b8bc1d66fe
juanpleo1983 <juanpleo1983@gmail.com>
2014-05-05 13:39:48 +0200

    XMLCancelarPedido añadido

--- RestauranteCamarero/src/prg/pi/restaurantecamarero/xml/XMLCancelarPedido.java
+++ RestauranteCamarero/src/prg/pi/restaurantecamarero/xml/XMLCancelarPedido.java
@@ -5,14 +5,15 @@ import XML.XML;
 
 public class XMLCancelarPedido extends XML{
 	
-	/* Borrar: me dijiste pedidoPendiente, pero creo que lo que necesito es pedidoListo */
+	/* Borrar: me dijiste pedidoPendiente, pero creo que lo que necesito es pedidoListo
+	 * como se va a hacer de uno en uno solo se le pasa idComanda y idMenu 
+	 */
 	
 	public XMLCancelarPedido(PedidoListo pedido){
 		init();
 		addNodo("tipo", "CancelarPedido", "paquete");
 		addNodo("idCom", pedido.getIdComanda()+"", "paquete");
 		addNodo("idMenu", pedido.getIdMenu()+"", "paquete");
-		addNodo("cantidad", pedido.getListos()+"", "paquete");
 	}
 
 }
