b9b1d8dbd937b6a500a2dda572dd2ef0fc1c496a
juanpleo1983 <juanpleo1983@gmail.com>
2014-06-03 21:25:46 +0200

    DecodificadorPedidosPendientesCamarero retocado

--- RestauranteCamarero/src/prg/pi/restaurantecamarero/decodificador/DecodificadorPedidosPendientesCamarero.java
+++ RestauranteCamarero/src/prg/pi/restaurantecamarero/decodificador/DecodificadorPedidosPendientesCamarero.java
@@ -46,29 +46,31 @@ public class DecodificadorPedidosPendientesCamarero {
 		NodeList nodeListMesa = dom.getElementsByTagName("mesa");
 		Node nodeMesa = nodeListMesa.item(0);
 		String nombreMesa = nodeMesa.getFirstChild().getNodeValue();
-		NodeList nodeListSeccion = dom.getElementsByTagName("seccion");
-		Node nodeSeccion = nodeListSeccion.item(0);
-		String nombreSeccion = nodeSeccion.getFirstChild().getNodeValue();
-		
-		/* Extraemos la información de los pedidos */
-		NodeList nodeListPedidos = dom.getElementsByTagName("pedidos");
-		Node nodePedidos = nodeListPedidos.item(0);
-		Element elementoPedidos = (Element) nodePedidos;
-		int idComanda = Integer.parseInt(elementoPedidos.getAttribute("idCom"));
-		
-		NodeList listaPedidos = dom.getElementsByTagName("pedido");
-		for(int contadorPedidos = 0; contadorPedidos < listaPedidos.getLength(); contadorPedidos++){
-			Node nodePedido = listaPedidos.item(contadorPedidos);
-			Element elementoPedido = (Element) nodePedido;
-			int idMenu = Integer.parseInt(elementoPedido.getAttribute("idMenu"));
-			NodeList nodeListDatos = nodePedido.getChildNodes();
-			String nombreProducto = nodeListDatos.item(0).getFirstChild().getNodeValue();
-            String nombreCantidad = nodeListDatos.item(1).getFirstChild().getNodeValue();
-            int unidades = Integer.parseInt(nodeListDatos.item(2).getFirstChild().getNodeValue());
-            int listos = Integer.parseInt(nodeListDatos.item(3).getFirstChild().getNodeValue());
-            int servidos = Integer.parseInt(nodeListDatos.item(4).getFirstChild().getNodeValue());
-            
-            pedidosPendientes.add(new PedidosPendientesCamarero(nombreSeccion, nombreMesa, idComanda, new Producto(idMenu, nombreProducto, nombreCantidad),unidades, listos, servidos));
+		if(!nombreMesa.equals("")){
+			NodeList nodeListSeccion = dom.getElementsByTagName("seccion");
+			Node nodeSeccion = nodeListSeccion.item(0);
+			String nombreSeccion = nodeSeccion.getFirstChild().getNodeValue();
+			
+			/* Extraemos la información de los pedidos */
+			NodeList nodeListPedidos = dom.getElementsByTagName("pedidos");
+			Node nodePedidos = nodeListPedidos.item(0);
+			Element elementoPedidos = (Element) nodePedidos;
+			int idComanda = Integer.parseInt(elementoPedidos.getAttribute("idCom"));
+			
+			NodeList listaPedidos = dom.getElementsByTagName("pedido");
+			for(int contadorPedidos = 0; contadorPedidos < listaPedidos.getLength(); contadorPedidos++){
+				Node nodePedido = listaPedidos.item(contadorPedidos);
+				Element elementoPedido = (Element) nodePedido;
+				int idMenu = Integer.parseInt(elementoPedido.getAttribute("idMenu"));
+				NodeList nodeListDatos = nodePedido.getChildNodes();
+				String nombreProducto = nodeListDatos.item(0).getFirstChild().getNodeValue();
+	            String nombreCantidad = nodeListDatos.item(1).getFirstChild().getNodeValue();
+	            int unidades = Integer.parseInt(nodeListDatos.item(2).getFirstChild().getNodeValue());
+	            int listos = Integer.parseInt(nodeListDatos.item(3).getFirstChild().getNodeValue());
+	            int servidos = Integer.parseInt(nodeListDatos.item(4).getFirstChild().getNodeValue());
+	            
+	            pedidosPendientes.add(new PedidosPendientesCamarero(nombreSeccion, nombreMesa, idComanda, new Producto(idMenu, nombreProducto, nombreCantidad),unidades, listos, servidos));
+			}
 		}
 	}
 }
